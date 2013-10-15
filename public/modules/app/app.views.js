324fbd91ab471d4e57edd31ba81802d391a81b08
seanbrookes <seanbrookes@beachair.ca>
2013-10-14 19:01:39 -0700

    auto save

--- public/modules/app/app.views.js
+++ public/modules/app/app.views.js
@@ -6,27 +6,27 @@
  * Time: 10:58 PM
  *
  */
-define(['backbone','marionette','modules/app/app.views','text!modules/app/app.templates.html'],
-    function(sf1, Marionette,View,template){
+define(['backbone', 'marionette', 'modules/app/app.views', 'text!modules/app/app.templates.html'],
+  function (sf1, Marionette, View, template) {
 
 
-        var BaseLayout = Backbone.Marionette.Layout.extend({
-                template:'#BaseTemplate'
+    var BaseLayout = Backbone.Marionette.Layout.extend({
+        template: '#BaseTemplate',
+        className: 'container'
 //                regions: {
 //                    appMainRegion: '.app-main',
 //                    appHeaderRegion:'.app-header',
 //                    appFooterRegion:'.app-footer',
 //                    mainNavRegion:'.main-nav'
 //                }
-            }
-        );
+      }
+    );
 
-        return {
-            BaseLayout:BaseLayout
+    return {
+      BaseLayout: BaseLayout
 
-        };
+    };
 
 
-
-    }
+  }
 );
